ea3fe7d7c14372ae5c9b5afd9e63f9d36c6593f3
Addy771 <addy771@gmail.com>
2025-03-22 18:18:44 -0700

    Update pinouts for V1 PCB

--- hw_def.h
+++ hw_def.h
@@ -33,27 +33,56 @@ UART0 RX | I2C0 SCL | SPI1 CSn | GP13 | 17│●              ●│24 | GP18   
                                                  └╌╌╌╌╌ SWCLK
 */
 
-#define UART_TX_GPIO 0          // UART0 TX default pin is GP0 (Pico pin 1)
-#define UART_RX_GPIO 1          // UART0 RX default pin is GP1 (Pico pin 2)
+/* Configuration */
 
-#define OLED_SDA_GPIO 4
-#define OLED_SCK_GPIO 5
-#define OLED_RST_GPIO 15
+#define UART_BAUDRATE 115200
+#define LOG_SAMPLE_RATE 10      // How often to sample data from VESC (in Hz)
 
-#define RTC_SDA_GPIO 10
-#define RTC_SCK_GPIO 11
-#define RTC_CE_GPIO 9
 
-#define DEBUG_GPIO 2            // Timing measurement debug output
+/* Pinout */
 
+// ESC UART communication
+#define UART_TX_GPIO 12             // UART0 TX
+#define UART_RX_GPIO 13             // UART0 RX
+
+// I2C bus for DS1307 RTC
+#define RTC_SDA_GPIO 20             // I2C0 SDA
+#define RTC_SCL_GPIO 27             // I2C0 SCL
+
+// ESC CAN bus transceiver
+#define CAN_TX_GPIO 8               // UART1 TX
+#define CAN_RX_GPIO 9               // UART1 RX
+
+// ST75256 LCD SPI interface
+#define DISPLAY_SDA_GPIO 3          // SPI0 SDA (MISO)
+#define DISPLAY_SCL_GPIO 4          // SPI0 SCL (CLK)
+#define DISPLAY_RST_GPIO 5          // Reset
+#define DISPLAY_CS_GPIO 6           // Chip Select
+#define DISPLAY_BACKLIGHT_GPIO 2    // Active-low PMOS backlight driver
+
+// SD card SPI bus
 #define SD_MISO_GPIO 16
 #define SD_MOSI_GPIO 19
 #define SD_SCK_GPIO 18
 #define SD_SS_GPIO 17
-#define SD_DETECT_GPIO 14
+#define SD_DETECT_GPIO 15
 
-#define PB_LEFT_GPIO 21         // Left-side pushbutton
-#define PB_RIGHT_GPIO 20        // Right-side pushbutton
+// Pushbutton GPIO
+#define PB_LEFT_GPIO 1
+#define PB_RIGHT_GPIO 2
+#define PB_CENTER_GPIO 22
 
-#define UART_BAUDRATE 115200
-#define LOG_SAMPLE_RATE 10      // How often to sample data from VESC (in Hz)
+// External load control
+#define EXT_LOAD_A_GPIO 11
+#define EXT_LOAD_B_GPIO 10
+
+// WS2812 LED signal output
+#define WS_LED_GPIO 7
+
+// Spare GPIO for debug
+#define DEBUG_GPIO 14
+
+// ADC pin numbers
+#define ADC_0_GPIO 26
+#define ADC_1_GPIO 27
+#define ADC_2_GPIO 28
